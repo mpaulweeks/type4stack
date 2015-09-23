a3378cc93a1162ab6f8941cbabcbe4c0a44e9f32
M. Paul Weeks <mpaulweeks@gmail.com>
2015-09-23 01:18:42 -0400

    working json loader

--- dumps/store.py
+++ dumps/store.py
@@ -1,3 +1,12 @@
 
+import json
+
+
 def load_cards():
-    
+    with open("data.json") as cards_file:
+        data = json.load(cards_file)
+    cards_by_id = {card["id"]: card for card in data["card"]}
+    status = data["status"]
+
+    print cards_by_id
+    print status
